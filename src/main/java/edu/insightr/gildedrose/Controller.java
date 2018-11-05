423da202b92ac0606824d96af2eb9ac01af3e366
Ruben <Ruben.amar2306@gmail.com>
2018-11-05 18:02:49 +0100

    Implémentation d'une fenêtre javafx dans le projet

---
+++ src/main/java/edu/insightr/gildedrose/Controller.java
@@ -0,0 +1,4 @@
+package edu.insightr.gildedrose;
+
+public class Controller {
+}
--- src/main/java/edu/insightr/gildedrose/Main.java
+++ src/main/java/edu/insightr/gildedrose/Main.java
@@ -1,3 +1,5 @@
+package edu.insightr.gildedrose;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
